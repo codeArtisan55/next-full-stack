2d51f8cfdcfc1968c3981855cae1a7627f640943
Awais <awaisotho8@gmail.com>
2024-09-23 10:32:49 -0700

    fix - stopped the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,26 @@ import type { NextRequest } from 'next/server'
 import { decode } from 'next-auth/jwt';
 import { cookies } from 'next/headers';
 export default async function middleware(request: NextRequest) {
-  const session = cookies().get("authjs.session-token");
-  const decodedSession = await decode({
-    token:session?.value,
-    secret:process.env.SECRET!,
-    salt:session?.name as string,
+  // const session = cookies().get("authjs.session-token");
+  // const decodedSession = await decode({
+  //   token:session?.value,
+  //   secret:process.env.SECRET!,
+  //   salt:session?.name as string,
 
-  })
-  if(decodedSession){
-    console.log("decodedSession", decodedSession);
-    alert("decodedSession")
+  // })
 
-  }
-alert("trying the middleware")
-  const pathname = request.nextUrl.pathname;
+  // const pathname = request.nextUrl.pathname;
 
-  const publicPaths = ["/signin", "/signup", "/verify-code"];
-  const isPublicPath = publicPaths.includes(pathname);
+  // const publicPaths = ["/signin", "/signup", "/verify-code"];
+  // const isPublicPath = publicPaths.includes(pathname);
 
-  if (decodedSession && isPublicPath) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  // if (decodedSession && isPublicPath) {
+  //   return NextResponse.redirect(new URL("/dashboard", request.url));
+  // }
 
-  if (!decodedSession && !isPublicPath) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
+  // if (!decodedSession && !isPublicPath) {
+  //   return NextResponse.redirect(new URL("/signin", request.url));
+  // }
 
 }
 
