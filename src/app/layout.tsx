801af73dbd74cfe214037fefaf01a41e92cff15d
Awais <awaisotho8@gmail.com>
2024-10-07 00:07:48 -0700

    fix- fixed the user route issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
-import AuthProviders from "@/context/authProvider"
-
-const inter = Inter({ subsets: ["latin"] });
+import AuthProviders from "@/context/authProvider";
 
 export const metadata: Metadata = {
   title: "Mystery Messages App",
-  description: "This is a mystery messages app where you can send messages anonymously",
-
+  description:
+    "This is a mystery messages app where you can send messages anonymously",
 };
 
 export default function RootLayout({
@@ -22,20 +19,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <AuthProviders>
-      <body className={inter.className}>
-        <Navbar/>
-      <ThemeProvider
+        <body>
+          <Navbar />
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            disableTransitionOnChange>
-              <div className="h-screen w-full overflow-hidden">
-
-        {children}
-              </div>
-        <Toaster />
-        </ThemeProvider>
-      </body>
+            disableTransitionOnChange
+          >
+            <div className="h-screen w-full overflow-hidden">{children}</div>
+            <Toaster />
+          </ThemeProvider>
+        </body>
       </AuthProviders>
     </html>
   );
