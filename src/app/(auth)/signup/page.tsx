74cf880ff27d236d43ff5b6cf7ddc173dbb49bab
Awais <awaisotho8@gmail.com>
2024-10-07 01:06:21 -0700

    style - add loader in home screen

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,13 @@
-"use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { useDebounceCallback } from 'usehooks-ts'
-import { useToast } from "@/components/ui/use-toast"
-import { signupSchema } from "@/schemas/SignUpSchema"
-import axios from "axios"
-import { apiResponse } from "@/types/apiResponse"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useDebounceCallback } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { signupSchema } from "@/schemas/SignUpSchema";
+import axios from "axios";
+import { apiResponse } from "@/types/apiResponse";
 
 import {
   Form,
@@ -16,176 +16,179 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Loader2 } from "lucide-react"
-import { useRouter } from "next/navigation"
-import Link from "next/link"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-
-
-
- const Register=()=>{
+const Register = () => {
   //state managements
-  const [userName,setUsername]=useState('')
-  const [userNameMessage,setUserNameMessage]=useState('')
-  const [checkingUsername,setCheckingUsername]=useState(false)
-  const [isformSubmitting,setIsformSubmitting]=useState(false)
-  const [isUserNameUnique,setisUserNameUnique]=useState(false)
-  const debounced=useDebounceCallback(setUsername, 500) // this is the debounced value , i will use this value in form and for backnd request
-  const { toast } = useToast()
-
+  const [userName, setUsername] = useState("");
+  const [userNameMessage, setUserNameMessage] = useState("");
+  const [checkingUsername, setCheckingUsername] = useState(false);
+  const [isformSubmitting, setIsformSubmitting] = useState(false);
+  const [isUserNameUnique, setisUserNameUnique] = useState(false);
+  const debounced = useDebounceCallback(setUsername, 500); // this is the debounced value , i will use this value in form and for backnd request
+  const { toast } = useToast();
 
   //   zod implementation
- const form= useForm({
-  resolver:zodResolver(signupSchema), // provide the schema
-  defaultValues:{
-    username:"",
-    email:'',
-    password:''
-  }
- })
+  const form = useForm({
+    resolver: zodResolver(signupSchema), // provide the schema
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
- const router=useRouter()
- useEffect(()=>{
-  const isUserNameUnique=async()=>{
-    if (userName) {
-      setCheckingUsername(true)
+  const router = useRouter();
+  useEffect(() => {
+    const isUserNameUnique = async () => {
+      if (userName) {
+        setCheckingUsername(true);
 
-      try {
-        const response=await axios.get(`/api/check-unique-username?username=${userName}`)
-      // setUserNameMessage(response.data?.message ?? "username checked")
-      // if(!response.data.success){
-        setUserNameMessage(response.data.message)
-        if (response.data.success) {
-          setisUserNameUnique(true)
-
-        }else{
-          setisUserNameUnique(false)
-
-
+        try {
+          const response = await axios.get(
+            `/api/check-unique-username?username=${userName}`
+          );
+          // setUserNameMessage(response.data?.message ?? "username checked")
+          // if(!response.data.success){
+          setUserNameMessage(response.data.message);
+          if (response.data.success) {
+            setisUserNameUnique(true);
+          } else {
+            setisUserNameUnique(false);
+          }
+        } catch (error) {
+          toast({
+            title: "failed",
+            description: "username unique check failed",
+          });
+        } finally {
+          setCheckingUsername(false); // success or false , but  setCheckingUsername , should be set false
         }
-
-
-
-      } catch (error) {
-        toast({
-          title: "failed",
-          description: "username unique check failed",
-        })
-
-
-      }finally{
-      setCheckingUsername(false) // success or false , but  setCheckingUsername , should be set false
-
       }
+    };
+    isUserNameUnique();
+  }, [userName]);
 
-    }
-  }
-  isUserNameUnique()
+  const onSubmit = async (data: z.infer<typeof signupSchema>) => {
+    setIsformSubmitting(true);
+    console.log(data);
 
- },[userName])
-
-const onSubmit=async (data:z.infer<typeof signupSchema>)=>{
-  setIsformSubmitting(true)
-  console.log(data);
-
-  try {
-    const response=await axios.post<apiResponse>('/api/signup',data)
-    if(response.data.success){
+    try {
+      const response = await axios.post<apiResponse>("/api/signup", data);
+      if (response.data.success) {
+        toast({
+          title: "success",
+          description: "user registration successful",
+        });
+      }
+      router.push(`/verify/${userName}`);
+    } catch (error) {
+      console.log(error);
       toast({
-        title:"success",
-        description:"user registration successful"
-      })
+        title: "failed",
+        description: "user registration failed",
+      });
+    } finally {
+      setIsformSubmitting(false);
     }
-    router.push(`/verify/${userName}`)
+  };
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen w-full ">
+        <div className="w-96 m-2 h-3/8 shadow-md rounded-md px-8  p-4 space-y-10 bg-white">
+          <div className="text-center">
+            <h2 className="text-3xl text-primary-foreground capitalize font-bold m-2 ">
+              Signup on Monkey
+            </h2>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="username"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          debounced(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    {checkingUsername && <Loader2 className="animate-spin" />}
+                    <p
+                      className={`text-sm ${
+                        isUserNameUnique ? "text-green-600" : "text-red-500"
+                      }`}
+                    >
+                      {userNameMessage}
+                    </p>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                variant="secondary"
+                type="submit"
+                disabled={isformSubmitting}
+              >
+                {isformSubmitting ? (
+                  <>
+                    <Loader2 className="animate-spin" /> please wait{" "}
+                  </>
+                ) : (
+                  "SignUp"
+                )}
+              </Button>
+            </form>
+            <div className="text-center ">
+              <h1 className="text-primary-foreground">
+                Already a member ?{" "}
+                <Link className="font-bold" href="/signin">
+                  signin
+                </Link>
+              </h1>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-  } catch (error) {
-    console.log(error);
-    toast({
-      title:"failed",
-      description:"user registration failed"
-    })
-  }finally{
-  setIsformSubmitting(false)
-
-  }
-
-
-}
-return (
-  <>
-<div className="flex items-center justify-center h-screen w-full ">
-  <div className="w-1/3 m-2 h-4/5 shadow-md rounded-md px-8  p-4 space-y-20">
-  <div className="text-center">
-  <h2 className="text-4xl capitalize font-bold m-2 ">Signup on Monkey</h2>
-
-  </div>
-  <Form  {...form} >
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-            <FormField
-  control={form.control}
-  name="username"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Username</FormLabel>
-      <FormControl>
-        <Input placeholder="username" {...field} onChange={(e)=>{
-          field.onChange(e)
-          debounced(e.target.value)
-        }} />
-      </FormControl>
-        {checkingUsername && (<Loader2 className="animate-spin" />)}
-        <p className={`text-sm ${isUserNameUnique? "text-green-600" : 'text-red-500'}`} >
-          {userNameMessage}
-        </p>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-  <FormField
-  control={form.control}
-  name="email"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>email</FormLabel>
-      <FormControl>
-        <Input placeholder="email" {...field}  />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-  <FormField
-  control={form.control}
-  name="password"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>password</FormLabel>
-      <FormControl>
-        <Input placeholder="password" {...field}  />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-<Button type="submit" disabled={isformSubmitting} >{isformSubmitting ? (<><Loader2 className="animate-spin"  /> please wait </> ):"SignUp"}</Button>
-</form>
-<div className="text-center ">
-  <h1>Already a member ? <Link className="font-bold" href='/signin'>signin</Link> </h1>
-
-</div>
-
-    </Form>
-
-  </div>
-
-</div>
-</>)
-
-}
-
-
-export default Register
+export default Register;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,34 @@
-"use client"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
-import messages from "@/data/messages.json"
-import MessageCard from "@/components/Card";
-import { SparklesCore } from "@/components/ui/sparkles";
+"use client";
 import {
   TextRevealCard,
   TextRevealCardDescription,
   TextRevealCardTitle,
 } from "@/components/ui/text-reveal-card";
 import Elelement from "@/components/Element";
-
+import { Suspense } from "react";
+import Loader from "@/components/Loader";
 
 export default function Home() {
-
-
   return (
     // <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-red-900 after:dark:via-[#5778da] after:dark:opacity-80 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
 
     //   </div>
-    <main className="relative  w-full h-full overflow-hidden">
-      <Elelement/>
-       {/* <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={50}
-          className="w-full h-full z-[-1] absolute"
-          particleColor="#FFFFFF"
-        /> */}
-      <section className="w-[80%] m-auto  p-10 text-center h-full">
-
-      <h1 className="text-8xl font-bold capitalize bg-gradient-to-r from-purple-800 via-pink-500 to-yellow-500 bg-clip-text text-transparent">
-        Welcome to the anonymous place
-      </h1>
-      <div className="flex items-start p-5 justify-center h-full  ">
-
-      <TextRevealCard className="text-8xl font-bold capital bg-transparent border-none line-clamp-4 leading-3 text-black " text="Sometimes, you just need to see it." revealText="I will show you">
-      </TextRevealCard>
-      </div>
-      {/* <div className="flex items-start p-5 justify-center h-full  ">
-        <Carousel className="w-1/2">
-          <CarouselContent>
-            {messages.map((message,index)=>{
-              return (<CarouselItem key={index}>
-                <MessageCard {...message}/>
-              </CarouselItem>)
-
-            })}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div> */}
-    </section>
-    </main>
+    <Suspense fallback={<Loader />}>
+      <main className="relative  w-full h-full overflow-hidden">
+        <Elelement />
+        <section className="w-[80%] m-auto  p-10 text-center h-full">
+          <h1 className="text-8xl font-bold capitalize bg-gradient-to-r from-purple-800 via-pink-500 to-yellow-500 bg-clip-text text-transparent">
+            Welcome to the anonymous place
+          </h1>
+          <div className="flex items-start p-5 justify-center h-full  ">
+            <TextRevealCard
+              className="text-8xl font-bold capital bg-transparent border-none line-clamp-4 leading-3 text-black "
+              text="Sometimes, you just need to see it."
+              revealText="I will show you"
+            ></TextRevealCard>
+          </div>
+        </section>
+      </main>
+    </Suspense>
   );
 }
